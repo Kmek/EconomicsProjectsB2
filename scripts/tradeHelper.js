34f4d8f0eb473763db43e75c878e7941d6868068
Kmek <kmekeefe@gmail.com>
2020-03-11 19:24:24 -0400

    Get current stock value using alpha vantage

--- scripts/tradeHelper.js
+++ scripts/tradeHelper.js
@@ -6,8 +6,9 @@ document.documentElement.style.setProperty('--minW', "300px")
 
 /******************** Constants ********************/
 var openLinksBtn = document.getElementById("openLinksBtn")
-var stockName = document.getElementById("stockName")
-var openStockBtn = document.getElementById("openStockBtn")
+var stockSymbol = document.getElementById("stockSymbol")
+var quoteStockBtn = document.getElementById("quoteStockBtn")
+var recentPrice = document.getElementById("recentPrice")
 var shares = document.getElementById("shares")
 var buy = document.getElementById("buyPrice")
 var commission = document.getElementById("commission")
@@ -36,10 +37,52 @@ calculateBtn.onclick = function() {
 // }
 
 /******************** Open Yahoo Finance Stock Page ********************/
-openStockBtn.onclick = function() {
-    if (stockName.value != "")
-        window.open("https://finance.yahoo.com/quote/" + stockName.value)
-}
+// openStockBtn.onclick = function() {
+//     if (stockSymbol.value != "")
+//         window.open("https://finance.yahoo.com/quote/" + stockSymbol.value)
+// }
 
-/******************** Using Stocks.js ********************/
+/******************** Using Alpha Vantage ********************/
 // Alpha Vantage Key: SFQX2ZC92AYNKDLR
+
+var keys
+
+// Function taken from https://github.com/prediqtiv/alpha-vantage-cookbook
+function requestStock(symbol) {
+    const url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + symbol + "&interval=1min&apikey=SFQX2ZC92AYNKDLR"
+
+    const xhr = new XMLHttpRequest();
+    xhr.open( 'GET', url, true );
+    xhr.onerror = function( xhr ) { console.log( 'error:', xhr  ); };
+    xhr.onprogress = function( xhr ) { console.log( 'bytes loaded:', xhr.loaded  ); }; /// or something
+    xhr.onload = callback;
+    xhr.send( null );
+
+    function callback( xhr ) {
+        let response, json;
+
+        response = xhr.target.response;
+        // divContents.innerText = response;
+
+        json = JSON.parse( response );
+        console.log(json)
+        json = json["Time Series (1min)"]
+
+        // Get keys
+        keys = []
+        for (key in json)
+            keys.push(key)
+
+        // console.log( 'json', keys[0], json[keys[0]]["4. close"])
+
+        // Set to recentPrice span
+        recentPrice.innerHTML = json[keys[0]]["4. close"]
+    }
+}
+// requestStock("AAPL");
+
+// Call on quote button
+quoteStockBtn.onclick = function() {
+    if (stockSymbol.value != "")
+        requestStock(stockSymbol.value)
+}
